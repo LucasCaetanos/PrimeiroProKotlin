8342105e2fb564cd40a547e61aff235fb831cbfa
LucasCaetanos <87659302+LucasCaetanos@users.noreply.github.com>
2021-10-22 11:45:20 -0300

    Subindo Primeiro projeto de Kotlin

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+
+    println("??? : Opa, eae")
+    println("Rogério: De boas?")
+    println("??? : Suaves")
+    println("Rogério: Qual é o seu nome?")
+    println("???: Meu nome é Henrique")
+    println("Rogério: Opa, prazer em conhecê-lo Henrique")
+    println("Henrique: O prazer é todo meu")
+}
